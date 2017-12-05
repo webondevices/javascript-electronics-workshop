d45425860cda8551891263af1b8a13dbdf504456
Mate Marschalko <webondevices@gmail.com>
2017-12-05 13:31:10 +0000

    Boilerplate for talking plant

---
+++ 14-talking-plant-starter/plant.js
@@ -0,0 +1,10 @@
+var five = require('johnny-five');
+var say = require('say');
+var faces = require('./faces.js');
+var util = require('./util.js');
+
+var arduino = new five.Board();
+
+arduino.on('ready', function () {
+    
+});
